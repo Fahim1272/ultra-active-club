b1cdbfcc73223c76d81b8d6377f313f5e338eb7f
Fahim1272 <imfahim72@gmail.com>
2022-09-30 08:21:50 +0600

    Card Updated

---
+++ src/components/home/Exercise/Card/Card.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import logo from '../../logo.png'
+import Cards from '../Cards/Cards';
+import './Card.css'
+const Card = (props) => {
+    const [data, setData] = useState([])
+    useEffect(()=>{
+        fetch('fakedata.json')
+        .then(res => res.json())
+        .then(data => setData(data))
+    }, [])
+    return (
+        <div>
+            <div className='logo'>
+                <img src={logo} alt="" />
+                <h2>Activity Tracker</h2>
+            </div>
+            <h3>Select Today's exercise</h3>
+            <div className='cardItems' data-aos="fade-up">
+                {
+                    data.map(item=> <Cards event={props.event} key={item.id} data={item}></Cards>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Card;
